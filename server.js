a91c08dee6474434505fb53383e5cf6e2a8fd56f
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-02-10 22:12:37 +0000

    Update server.js to "standard" JS style

--- server.js
+++ server.js
@@ -1,52 +1,57 @@
-var express = require('express'),
-    logger = require('morgan'),
-    pug = require('pug'),
-    fs = require('fs');
+'use strict'
 
-var templateDirectory = __dirname + '/source/templates/'
-var homepage = pug.compileFile(templateDirectory + 'homepage.pug')
-var projectPage = pug.compileFile(templateDirectory + 'project.pug')
-var projectsPage = pug.compileFile(templateDirectory + 'projects.pug')
+const path = require('path')
 
-var projects = JSON.parse(fs.readFileSync(__dirname + '/source/json/projects.json'));
+var express = require('express')
+var logger = require('morgan')
+var pug = require('pug')
+var fs = require('fs')
+
+var templateDirectory = path.join(__dirname, '/source/templates/')
+var homepage = pug.compileFile(path.join(templateDirectory, 'homepage.pug'))
+var projectPage = pug.compileFile(path.join(templateDirectory + 'project.pug'))
+var projectsPage = pug.compileFile(path.join(templateDirectory + 'projects.pug'))
+
+var projects = JSON.parse(fs.readFileSync(path.join(__dirname, '/source/json/projects.json')))
 
 var app = express()
+
 app.use(logger('dev'))
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(path.join(__dirname, '/static')))
 
 app.get('/', function (req, res, next) {
-    try {
-        res.send(homepage())
-    } catch (e) {
-        next(e)
-    }
+  try {
+    res.send(homepage())
+  } catch (e) {
+    next(e)
+  }
 })
 
 app.get('/projects', function (req, res, next) {
-    try {
-        var html = projectsPage({
-            "projects": projects
-        })
-        res.send(html)
-    } catch (e) {
-        next(e)
-    }
+  try {
+    var html = projectsPage({
+      'projects': projects
+    })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
 })
 
 app.get('/projects/:project', function (req, res, next) {
-    try {
-        var project = req.params.project;
-        var html = projectPage({
-            "project": projects.projects[project]
-        })
-        res.send(html)
-    } catch (e) {
-        next(e)
-    }
+  try {
+    var project = req.params.project
+    var html = projectPage({
+      'project': projects.projects[project]
+    })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
 })
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1337
 
 app.listen(port, function () {
-    console.log('Listening on http://localhost:' + port)
+  console.log('Listening on http://localhost:' + port)
 })
