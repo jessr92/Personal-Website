f3fe220813fd8b0f2aa342bc91220b31dd3916d4
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-02-13 21:11:21 +0000

    Adds custom 404 and 500 plus reworks routing

--- src/js/routing.js
+++ src/js/routing.js
@@ -7,7 +7,11 @@ var htmlStore = require('./htmlStore')
 
 var handleRequest = function (url, res, next) {
   try {
-    res.send(htmlStore.get(url))
+    if (htmlStore.has(url)) {
+      res.send(htmlStore.get(url))
+    } else {
+      next()
+    }
   } catch (e) {
     next(e)
   }
@@ -23,6 +27,15 @@ router.get('/projects', function (req, res, next) {
 
 router.get('/projects/:project', function (req, res, next) {
   handleRequest(req.url, res, next)
+})
+
+router.use(function (req, res, next) {
+  handleRequest('$404$', res.status(404), next)
+})
+
+router.use(function (err, req, res, next) {
+  console.error(err.stack)
+  handleRequest('$500$', res.status(500), next)
 })
 
 module.exports = router
