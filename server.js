c8e920ba7979291ec694d99708f752ba46ca0bb0
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-02-13 19:55:49 +0000

    Adds helmet for security

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 var path = require('path')
 var express = require('express')
 var logger = require('morgan')
+var helmet = require('helmet')
 
 var htmlStore = require('./src/js/htmlStore.js')
 
 var app = express()
 
+app.use(helmet())
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, '/static')))
 
