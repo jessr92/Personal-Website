4a0265becc7bd9d0bbce6579ae7a270923523803
Jessica Reid <jess.reid1992@outlook.com>
2017-02-13 21:11:21 +0000

    Adds custom 404 and 500 plus reworks routing

--- src/js/htmlStore.js
+++ src/js/htmlStore.js
@@ -1,39 +1,60 @@
 'use strict'
 
-module.exports = (function () {
-  var path = require('path')
-  var pug = require('pug')
-  var fs = require('fs')
+var path = require('path')
+var pug = require('pug')
+var fs = require('fs')
 
-  var htmlStore = {}
+var templateDirectory = path.join(__dirname, '../templates/')
+var htmlStore = {}
 
-  var init = function () {
-    var templateDirectory = path.join(__dirname, '../templates/')
-    var homepage = pug.compileFile(path.join(templateDirectory, 'homepage.pug'))
-    var projectPage = pug.compileFile(path.join(templateDirectory + 'project.pug'))
-    var projectsPage = pug.compileFile(path.join(templateDirectory + 'projects.pug'))
-    var projects = JSON.parse(fs.readFileSync(path.join(__dirname, '../json/projects.json')))
+var addErrorPages = function () {
+  var errorNotFoundPage = pug.compileFile(path.join(templateDirectory, '404.pug'))
+  var internalServerErrorPage = pug.compileFile(path.join(templateDirectory, '500.pug'))
+  htmlStore['$404$'] = errorNotFoundPage()
+  htmlStore['$500$'] = internalServerErrorPage()
+}
 
-    htmlStore['/'] = homepage()
-    htmlStore['/projects'] = projectsPage({
-      'projects': projects
+var addProjectPages = function () {
+  var projectPage = pug.compileFile(path.join(templateDirectory + 'project.pug'))
+  var projectsPage = pug.compileFile(path.join(templateDirectory + 'projects.pug'))
+  var projects = JSON.parse(fs.readFileSync(path.join(__dirname, '../json/projects.json')))
+
+  htmlStore['/projects'] = projectsPage({
+    'projects': projects
+  })
+  for (var idx in projects.ids) {
+    var id = projects.ids[idx]
+    htmlStore['/projects/' + id] = projectPage({
+      'project': projects.projects[id]
     })
-    for (var idx in projects.ids) {
-      var id = projects.ids[idx]
-      htmlStore['/projects/' + id] = projectPage({
-        'project': projects.projects[id]
-      })
-    }
   }
+}
 
-  var storeAccessor = function () {}
-  storeAccessor.get = function (pageId) {
-    if (pageId in htmlStore) {
-      return htmlStore[pageId]
-    }
-  }
+var addStaticPages = function () {
+  var homepage = pug.compileFile(path.join(templateDirectory, 'homepage.pug'))
+  htmlStore[''] = homepage()
+}
 
+var init = function () {
+  addErrorPages()
+  addProjectPages()
+  addStaticPages()
+}
+
+var getHtmlStore = function () {
   init()
 
+  var storeAccessor = function () {}
+
+  storeAccessor.get = function (pageId) {
+    return htmlStore[pageId.replace(/\/$/, '')]
+  }
+
+  storeAccessor.has = function (pageId) {
+    return pageId.replace(/\/$/, '') in htmlStore
+  }
+
   return storeAccessor
-})()
+}
+
+module.exports = getHtmlStore()
