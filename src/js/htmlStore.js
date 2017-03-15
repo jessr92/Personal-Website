c9b105d1859910f87483be3a4c97a0361e9c2914
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-03-15 21:48:47 +0000

    Refactor JS and make use of JSHint

--- src/js/htmlStore.js
+++ src/js/htmlStore.js
@@ -1,60 +1,62 @@
-'use strict'
+/*jshint node: true*/
+"use strict";
 
-var path = require('path')
-var pug = require('pug')
-var fs = require('fs')
+var path = require("path");
+var pug = require("pug");
+var fs = require("fs");
 
-var templateDirectory = path.join(__dirname, '../templates/')
-var htmlStore = {}
+var templateDirectory = path.join(__dirname, "../templates/");
+var htmlStore = {};
+var defaultPugOptions = null;
 
 var addErrorPages = function () {
-  var errorNotFoundPage = pug.compileFile(path.join(templateDirectory, '404.pug'))
-  var internalServerErrorPage = pug.compileFile(path.join(templateDirectory, '500.pug'))
-  htmlStore['$404$'] = errorNotFoundPage()
-  htmlStore['$500$'] = internalServerErrorPage()
-}
+    var errorNotFoundPage = pug.compileFile(path.join(templateDirectory, "404.pug"), defaultPugOptions);
+    var internalServerErrorPage = pug.compileFile(path.join(templateDirectory, "500.pug"), defaultPugOptions);
+    htmlStore["404_ERROR_PAGE"] = errorNotFoundPage();
+    htmlStore["500_ERROR_PAGE"] = internalServerErrorPage();
+};
 
+/**
+ * @property {array} projects.ids   Array of unique IDs that represent each coding project.
+ */
 var addProjectPages = function () {
-  var projectPage = pug.compileFile(path.join(templateDirectory, 'project.pug'))
-  var projectsPage = pug.compileFile(path.join(templateDirectory, 'projects.pug'))
-  var projects = JSON.parse(fs.readFileSync(path.join(__dirname, '../json/projects.json')))
+    var projectPage = pug.compileFile(path.join(templateDirectory, "project.pug"), defaultPugOptions);
+    var projectsPage = pug.compileFile(path.join(templateDirectory, "projects.pug"), defaultPugOptions);
+    var projectsJSON = fs.readFileSync(path.join(__dirname, "../json/projects.json"));
+    var projects = JSON.parse(projectsJSON);
 
-  htmlStore['/projects'] = projectsPage({
-    'projects': projects
-  })
-  for (var idx in projects.ids) {
-    var id = projects.ids[idx]
-    htmlStore['/projects/' + id] = projectPage({
-      'project': projects.projects[id]
-    })
-  }
-}
+    htmlStore["/projects"] = projectsPage({
+        "projects": projects
+    });
+    projects.ids.forEach(function (id) {
+        htmlStore["/projects/" + id] = projectPage({
+            "project": projects.projects[id]
+        });
+    });
+};
 
 var addStaticPages = function () {
-  var homepage = pug.compileFile(path.join(templateDirectory, 'homepage.pug'))
-  htmlStore[''] = homepage()
-}
+    var homepage = pug.compileFile(path.join(templateDirectory, "homepage.pug"), defaultPugOptions);
+    htmlStore[""] = homepage();
+};
 
 var init = function () {
-  addErrorPages()
-  addProjectPages()
-  addStaticPages()
-}
+    addErrorPages();
+    addProjectPages();
+    addStaticPages();
+};
 
 var getHtmlStore = function () {
-  init()
+    init();
 
-  var storeAccessor = function () {}
+    return {
+        get: function (pageId) {
+            return htmlStore[pageId.replace(/\/$/, "")];
+        },
+        has: function (pageId) {
+            return htmlStore[pageId.replace(/\/$/, "")] !== undefined;
+        }
+    };
+};
 
-  storeAccessor.get = function (pageId) {
-    return htmlStore[pageId.replace(/\/$/, '')]
-  }
-
-  storeAccessor.has = function (pageId) {
-    return pageId.replace(/\/$/, '') in htmlStore
-  }
-
-  return storeAccessor
-}
-
-module.exports = getHtmlStore()
+module.exports = getHtmlStore();
--- src/js/routing.js
+++ src/js/routing.js
@@ -1,41 +1,42 @@
-'use strict'
+/*jshint node: true*/
+"use strict";
 
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = new express.Router();
 
-var htmlStore = require('./htmlStore')
+var htmlStore = require("./htmlStore");
 
 var handleRequest = function (url, res, next) {
-  try {
-    if (htmlStore.has(url)) {
-      res.send(htmlStore.get(url))
-    } else {
-      next()
+    try {
+        if (htmlStore.has(url)) {
+            res.send(htmlStore.get(url));
+        } else {
+            next();
+        }
+    } catch (e) {
+        next(e);
     }
-  } catch (e) {
-    next(e)
-  }
-}
+};
 
-router.get('/', function (req, res, next) {
-  handleRequest(req.url, res, next)
-})
+router.get("/", function (req, res, next) {
+    handleRequest(req.url, res, next);
+});
 
-router.get('/projects', function (req, res, next) {
-  handleRequest(req.url, res, next)
-})
+router.get("/projects", function (req, res, next) {
+    handleRequest(req.url, res, next);
+});
 
-router.get('/projects/:project', function (req, res, next) {
-  handleRequest(req.url, res, next)
-})
+router.get("/projects/:project", function (req, res, next) {
+    handleRequest(req.url, res, next);
+});
 
-router.use(function (req, res, next) {
-  handleRequest('$404$', res.status(404), next)
-})
+router.use(function (ignore, res, next) {
+    handleRequest("404_ERROR_PAGE", res.status(404), next);
+});
 
-router.use(function (err, req, res, next) {
-  console.error(err.stack)
-  handleRequest('$500$', res.status(500), next)
-})
+router.use(function (err, ignore, res, next) {
+    console.error(err.stack);
+    handleRequest("500_ERROR_PAGE", res.status(500), next);
+});
 
-module.exports = router
+module.exports = router;
