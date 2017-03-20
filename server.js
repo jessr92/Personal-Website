1f36dfcb672297e0022d0c7536344f0673197181
Jessica Reid <jess.reid1992@outlook.com>
2017-03-20 09:18:02 +0000

    Update server.js to have no JSHint warnings

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-'use strict'
+/*jshint node: true*/
+"use strict";
 
-var app = require('./src/js/express')
-var port = process.env.PORT || 1337
+var app = require('./src/js/express');
+var port = process.env.PORT || 1337;
 
 app.listen(port, function () {
-  console.log('Listening on http://localhost:' + port)
-})
+    console.log('Listening on http://localhost:' + port)
+});
