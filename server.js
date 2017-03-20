335b036fb191c13a95e1175f833f74465b956a86
Jessica Reid <jess.reid1992@outlook.com>
2017-03-20 21:46:13 +0000

    Adds missing semi colon in server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var app = require('./src/js/express');
 var port = process.env.PORT || 1337;
 
 app.listen(port, function () {
-    console.log('Listening on http://localhost:' + port)
+    console.log('Listening on http://localhost:' + port);
 });
