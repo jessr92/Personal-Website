f239c72ed2fa8b93111d988c279b119660080dd3
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2017-03-20 22:21:36 +0000

    Runs formatter over code and resolves warnings

--- test/htmlStore.test.js
+++ test/htmlStore.test.js
@@ -1,33 +1,32 @@
 /* eslint-env mocha */
+/*jshint node: true, mocha: true*/
+"use strict";
 
-'use strict'
+var assert = require("assert");
+var path = require("path");
+var fs = require("fs");
+var htmlStore = require("../src/js/htmlStore");
 
-var assert = require('assert')
-var path = require('path')
-var fs = require('fs')
-var htmlStore = require('../src/js/htmlStore')
-
-describe('HTML Store', function () {
-  describe('get', function () {
-    it('should return HTML for homepage', function () {
-      var html = htmlStore.get('/')
-      assert(html.length > 0)
-    })
-    it('should return HTML for projects page', function () {
-      var html = htmlStore.get('/projects')
-      assert(html.length > 0)
-    })
-    it('should return HTML for each project', function () {
-      var projects = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/json/projects.json')))
-      for (var idx in projects.ids) {
-        var id = projects.ids[idx]
-        var html = htmlStore.get('/projects/' + id)
-        assert(html.length > 0)
-      }
-    })
-    it('should not return HTML for a non-existent page', function () {
-      var html = htmlStore.get('/non-existent')
-      assert(html === undefined)
-    })
-  })
-})
+describe("HTML Store", function () {
+    describe("get", function () {
+        it("should return HTML for homepage", function () {
+            var html = htmlStore.get("/");
+            assert(html.length > 0);
+        });
+        it("should return HTML for projects page", function () {
+            var html = htmlStore.get("/projects");
+            assert(html.length > 0);
+        });
+        it("should return HTML for each project", function () {
+            var projects = JSON.parse(fs.readFileSync(path.join(__dirname, "../src/json/projects.json")));
+            projects.ids.forEach(function (id) {
+                var html = htmlStore.get("/projects/" + id);
+                assert(html.length > 0);
+            });
+        });
+        it("should not return HTML for a non-existent page", function () {
+            var html = htmlStore.get("/non-existent");
+            assert(html === undefined);
+        });
+    });
+});
