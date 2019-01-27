8599c9e5608b3d1e205ce8cfbbea930db712f224
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2019-01-27 13:59:12 +0000

    Migrate to static website.
    I have sufficiently little content that NodeJS is over the top

--- server.js
+++
@@ -1,9 +0,0 @@
-/*jshint node: true, esversion: 6 */
-"use strict";
-
-const app = require('./src/js/express');
-const port = process.env.PORT || 1337;
-
-app.listen(port, function () {
-    console.log('Listening on http://localhost:' + port);
-});
