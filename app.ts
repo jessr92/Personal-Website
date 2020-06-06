93e10588746aa68518cfe5ab8d635506028981b2
Jessica Reid <jess.reid1992@outlook.com>
2020-06-06 10:35:58 +0100

    Rewrite in Node

---
+++ app.ts
@@ -0,0 +1,31 @@
+import express = require('express');
+import favicon = require('serve-favicon');
+import path = require('path');
+
+const projects: { [key:string]: any} = require('./app/projects.json');
+
+const app: express.Application = express();
+
+app.set('view engine', 'pug')
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/projects', function (req, res) {
+    res.render('projects', {"projects": projects});
+});
+
+app.get('/projects/:project_id', function (req, res) {
+    const project_id: string = req.params['project_id'];
+    res.render('project', projects[project_id]);
+});
+
+const port: number = Number.parseInt(process.env.PORT || "3000");
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
