8c0340f19d0a8fa8f2f5b336cd09d6621a545323
Jessica Reid <jess.reid1992@outlook.com>
2020-06-06 18:01:05 +0100

    app_test.ts: Create single test that checks / returns 200

---
+++ test/app_test.ts
@@ -0,0 +1,20 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
+
+chai.use(chaiHttp)
+chai.should();
+
+describe("Website", () => {
+    describe("GET /", () => {
+        it("should load homepage", (done) => {
+           chai.request(app)
+               .get('/')
+               .end((err, res) => {
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   done();
+               })
+        });
+    });
+});
