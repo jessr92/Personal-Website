399c981deb2494fcac5c014f0bc2bbdc9dc8af30
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2020-06-14 15:25:55 +0100

    Removes AWS CodeDeploy scripts
    Not supported on Ubuntu 20.04 yet.

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-apt update && apt full-upgrade -y
-
-# TODO install node
-
-npm install -g pm2
-pm2 update
--- scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/ubuntu/Personal-Website || exit
-pm2 stop "Personal Website" || true
