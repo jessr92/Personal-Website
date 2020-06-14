03a177bde9b7903c3551b7f3ae5cae75de663228
Jessica Reid <jess.reid1992@outlook.com>
2020-06-14 15:54:22 +0100

    app.ts: Return custom 404 when requesting project or page that does not exist

--- test/app_test.ts
+++ test/app_test.ts
@@ -14,7 +14,40 @@ describe("Website", () => {
                     res.should.have.status(200);
                     res.body.should.be.a("object");
                     done();
-                })
+                });
+        });
+    });
+    describe("GET /projects", () => {
+        it("should load projects list", (done) => {
+            chai.request(app)
+                .get("/projects")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    done();
+                });
+        });
+    });
+    describe("GET /projects/LES", () => {
+        it("should load project page", (done) => {
+            chai.request(app)
+                .get("/projects/LES")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    done();
+                });
+        });
+    });
+    describe("GET /projects/does-not-exist", () => {
+        it("should load 404 page", (done) => {
+            chai.request(app)
+                .get("/projects/does-not-exist")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a("object");
+                    done();
+                });
         });
     });
 });
