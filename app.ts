f1251710ab2d88ccbe236723eef58891d163143c
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2020-06-14 15:54:22 +0100

    app.ts: Return custom 404 when requesting project or page that does not exist

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express = require("express");
 import favicon = require("serve-favicon");
 import path = require("path");
 
-const projects: { [key:string]: any} = require(path.join(__dirname, "app", "projects.json"));
+const projects: { [key: string]: any } = require(path.join(__dirname, "app", "projects.json"));
 
 const app: express.Application = express();
 
@@ -20,9 +20,17 @@ app.get("/projects", function (req, res) {
     res.render("projects", {"projects": projects});
 });
 
-app.get("/projects/:project_id", function (req, res) {
+app.get("/projects/:project_id", function (req, res, next) {
     const project_id: string = req.params["project_id"];
-    res.render("project", projects[project_id]);
+    if (project_id in projects) {
+        res.render("project", projects[project_id]);
+    } else {
+        next();
+    }
+});
+
+app.use(function (req, res) {
+    res.status(404).render("404");
 });
 
 const port: number = Number.parseInt(process.env.PORT || "3000");
