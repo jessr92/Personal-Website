22a0cebd0e9335edb682f5898f680d7733fd86f7
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2021-01-30 15:25:05 +0000

    Auto discover files defining pages

--- constants.ts
+++ constants.ts
@@ -1,11 +1,7 @@
-import {HtmlOutputPathToTemplateMetadata} from "./custom_types";
-
 export const OUTPUT_FOLDER = '../gordon1992.github.io/';
+export const PAGES_FOLDER = './pages/';
 export const RESOURCES_FOLDER = './resources/';
 export const VIEWS_FOLDER = './views/';
 export const PUG_OPTIONS = {
     pretty: true
 };
-export const BASE_PAGES: HtmlOutputPathToTemplateMetadata = require("./pages/base.json");
-export const INTERESTS_PAGES: HtmlOutputPathToTemplateMetadata = require("./pages/interests.json");
-export const PROJECTS_PAGES: HtmlOutputPathToTemplateMetadata = require("./pages/projects.json");
