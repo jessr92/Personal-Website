802928e231a60e8a0cf67bd29e9519edeea902fc
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2021-02-03 21:36:30 +0000

    Remove onclick, instead use event handles. Tidy up CSS too

--- resources/js/imageGallery.js
+++ resources/js/imageGallery.js
@@ -1,32 +1,43 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+(function (exports) {
+    let slideIndex = 1;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    function nextSlide() {
+        showSlide(slideIndex + 1);
+    }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    function previousSlide() {
+        showSlide(slideIndex - 1);
+    }
 
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("slide-container");
-    const thumbnails = document.getElementsByClassName("thumbnail");
-    const captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1
+    function showSlide(n) {
+        slideIndex = n;
+        const slides = document.getElementsByClassName("slide-container");
+        const thumbnails = document.getElementsByClassName("thumbnail");
+        const captionText = document.getElementById("caption");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < thumbnails.length; i++) {
+            thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        thumbnails[slideIndex - 1].className += " active";
+        captionText.innerHTML = thumbnails[slideIndex - 1].alt;
     }
-    if (n < 1) {
-        slideIndex = slides.length
+
+    exports.setupSlides = function () {
+        showSlide(1);
+        document.querySelector('#prevButton').addEventListener('click', previousSlide);
+        document.querySelector('#nextButton').addEventListener('click', nextSlide);
+        document.querySelectorAll('.thumbnail').forEach((thumbnail, index) => {
+            thumbnail.addEventListener('click', () => showSlide(index + 1));
+        });
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < thumbnails.length; i++) {
-        thumbnails[i].className = thumbnails[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    thumbnails[slideIndex - 1].className += " active";
-    captionText.innerHTML = thumbnails[slideIndex - 1].alt;
-}
+})(window);
+window.setupSlides();
