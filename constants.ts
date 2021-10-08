9ef8d5dae261a42f45d9db654d000c96814e4f05
Jessica Reid <jess.reid1992@outlook.com>
2021-10-08 14:10:13 +0100

    Use pretty package rather than Pug's pretty printer
    Pug is deprecating the pretty printer due to bugs (not that I have experienced any).
    
    No change to output HTML with pretty package.

--- constants.ts
+++ constants.ts
@@ -2,6 +2,4 @@ export const OUTPUT_FOLDER = '../gordon1992.github.io/';
 export const PAGES_FOLDER = './pages/';
 export const RESOURCES_FOLDER = './resources/';
 export const VIEWS_FOLDER = './views/';
-export const PUG_OPTIONS = {
-    pretty: true
-};
+export const PUG_OPTIONS = {};
