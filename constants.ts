eee09fecf3d338b0a6d9fd4a04c10dbb6aada6d2
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2024-02-01 20:08:51 +0000

    Website name update

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const OUTPUT_FOLDER = '../gordon1992.github.io/';
+export const OUTPUT_FOLDER = '../jessr92.github.io/';
 export const PAGES_FOLDER = './pages/';
 export const RESOURCES_FOLDER = './resources/';
 export const VIEWS_FOLDER = './views/';
